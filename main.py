2a0ddbfd8c9aa616368a2f0b28a9607be815c16a
17altaka <d.andres.1205@gmail.com>
2024-03-23 12:04:00 -0500

    diccionario

---
+++ main.py
@@ -0,0 +1,7 @@
+victor_data = {"name": "victor", "high": 183, "birthday": "1977-10-04", "zodiac": "libra":, "hobby":, "video games"}
+juan_data = {"name": "juan", "high": 170, "birthday": "2000-16-09", "zodiac": "geminis":, "hobby":, "play soccer"}
+valentina_data = {"name": "", "high": 155, "birthday": "2002-18-07", "zodiac": "tauro":, "hobby":, "cook"}
+
+person_dict = {"1123456765": victor_data,"1123543234": juan_data, "2234654765": valentina_data}
+print(person_dict)
+print(person_dict)
